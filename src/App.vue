b02d6cda2e6b5a75d32523670b9fa6b8dd3429e4
iknowca <iknowca@gmail.com>
2024-01-31 23:11:51 +0900

    ✨design: app bar flat으로 수정 [IMF-2]

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,21 @@
 <template>
   <v-app app>
-    <v-layout>
-      <AppBar></AppBar>
-      <v-main>
-        <router-view/>
-      </v-main>
-    </v-layout>
+    <AppBar></AppBar>
+    <v-main>
+      <v-container>
+        <router-view />
+      </v-container>
+    </v-main>
 
   </v-app>
 </template>
 
 <script setup>
 import AppBar from './components/appbar/AppBar.vue'
+
+import AOS from 'aos';
+
+AOS.init();
 
 
 </script>
