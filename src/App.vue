57f51f0e8134f4adba72feb7e909534ce3d5e527
iknowca <iknowca@gmail.com>
2024-02-01 01:29:54 +0900

    💄feat: footer 추가 [IMF-9]

--- src/App.vue
+++ src/App.vue
@@ -6,13 +6,13 @@
         <router-view />
       </v-container>
     </v-main>
-
+    <FooterBar></FooterBar>
   </v-app>
 </template>
 
 <script setup>
 import AppBar from './components/appbar/AppBar.vue'
-
+import FooterBar from './components/footer/FooterBar.vue'
 import AOS from 'aos';
 
 AOS.init();
---
+++ src/components/footer/FooterBar.vue
@@ -0,0 +1,38 @@
+<template lang="">
+    <div>
+        <v-footer>
+    <v-row justify="center" no-gutters>
+      <v-col class="text-center" cols="12">
+      이 페이지의 소스코드가 궁금하시다면
+      </v-col>
+      <div>
+        <a href="https://github.com/iknowca/iknow-main-frontend">
+      <v-btn flat>
+        FRONTEND
+        <v-icon>mdi-github</v-icon>
+      </v-btn>
+    </a>
+      <a href="https://github.com/iknowca/iknow-authentication-server">
+      <v-btn flat>
+        BACKEND
+        <v-icon>mdi-github</v-icon>
+      </v-btn>
+    </a>
+      </div>
+    
+      <v-col class="text-center mt-4" cols="12">
+        {{ new Date().getFullYear() }} — <strong>IKNOW</strong>
+      </v-col>
+    </v-row>
+  </v-footer>
+    </div>
+</template>
+<script setup>
+
+</script>
+<style scope>
+a {
+  color: inherit;
+  text-decoration: none;
+}
+</style>
