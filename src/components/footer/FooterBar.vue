52916e8ffb0aad3453a2d3203c89a0a734622b19
iknowca <iknowca@gmail.com>
2024-02-02 13:29:05 +0900

    💄design: footer 디자인 변경 [IMF-2]

--- src/components/footer/FooterBar.vue
+++ src/components/footer/FooterBar.vue
@@ -1,7 +1,18 @@
 <template lang="">
     <div>
         <v-footer>
+   
     <v-row justify="center" no-gutters>
+      <v-container></v-container>
+      <v-col cols="12">
+        <div class="footerBanner ma-0 text-center d-flex align-center justify-center">
+          <div>
+          <h1>절찬리 취업중</h1>
+          <h2  @click="router.push('/contact')">연락하러 가기<v-icon>mdi-arrow-right-bold-circle-outline</v-icon></h2>
+        </div>
+        </div>
+      </v-col>
+      <v-container></v-container>
       <v-col class="text-center" cols="12">
       이 페이지의 소스코드가 궁금하시다면
       </v-col>
@@ -28,11 +39,19 @@
     </div>
 </template>
 <script setup>
-
+import { useRouter } from 'vue-router';
+const router = useRouter()
 </script>
 <style scope>
 a {
   color: inherit;
   text-decoration: none;
+}
+
+div.footerBanner {
+  background-color: aliceblue;
+  height: 150px;
+  width: 100%;
+  display: block;
 }
 </style>
