2f8abef63cf2d07820fcdadcad233f649b7bed59
iknowca <iknowca@gmail.com>
2024-02-02 13:27:07 +0900

    💄add: 메인페이지 수정 [IMF-9]

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -75,7 +75,7 @@
 
           <v-carousel-item>
             <div class="ma-4">
-              <v-card width="70%" class="mx-auto" height="670px">
+              <v-card width="70%" class="mx-auto" max-width="400px">
 
                 <v-card-text>
                   <v-img src="../assets/blog2.png" class="rounded-xl" width="100%" style="object-fit: contain;"></v-img>
@@ -84,22 +84,24 @@
                   <h2>블로그 포스팅</h2>
                 </v-card-title>
                 <div class="pa-4">
-                  <p>공부하면서 궁굼했던 걸 확인해본 결과,<br /> 공유하고 싶은 경험, 지식들을 포스팅 하고 있어요</p>
+                  <p>공부하면서 궁금했던 걸 확인해본 결과,<br />공유하고 싶은 경험, 지식들을 포스팅 하고 있어요</p>
                 </div>
                 <v-card-actions>
-                    <v-btn color="primary" :href="`https://velog.io/@iknow/posts`" target="_black" style="margin-left: auto;">블로그 보러가기</v-btn>
+                  <v-btn color="primary" :href="`https://velog.io/@iknow/posts`" target="_black"
+                    style="margin-left: auto;">블로그 보러가기</v-btn>
                 </v-card-actions>
               </v-card>
             </div>
           </v-carousel-item>
           <v-carousel-item>
             <div class="ma-4">
-              <v-card width="70%" class="mx-auto" height="670px">
+              <v-card width="70%" class="mx-auto" max-width="400px">
 
-                  <v-img src="../assets/codingtest.png" class="rounded-xl" width="100%" style="object-fit: contain;"></v-img>
-                  <v-container></v-container>
-                  <v-container></v-container>
-                  <h2>코딩 테스트</h2>
+                <v-img src="../assets/codingtest.png" class="rounded-xl" width="100%"
+                  style="object-fit: contain;"></v-img>
+                <v-container></v-container>
+                <v-container></v-container>
+                <h2>코딩 테스트</h2>
                 <div class="pa-4">
                   <p>코딩테스트 문제를 해결해 나가며,<br />부족했던 알고리즘 지식들을 채우고 있어요</p>
                 </div>
@@ -108,14 +110,14 @@
           </v-carousel-item>
           <v-carousel-item>
             <div class="ma-4">
-              <v-card width="70%" class="mx-auto" height="670px">
+              <v-card class="mx-auto" max-width="400px">
 
-                  <v-img src="" alt="스터디 사진" class="rounded-xl" width="100%" style="object-fit: contain;"></v-img>
-                  <v-container></v-container>
-                  <v-container></v-container>
-                  <h2>정기 스터디</h2>
+                <v-img src="" alt="스터디 사진" class="rounded-xl" ></v-img>
+                <v-container></v-container>
+                <v-container></v-container>
+                <h2>정기 스터디</h2>
                 <div class="pa-4">
-                  <p>매주 정기 스터디를 통해,<br>정해진 주제에 대한 발표,<br />배경지식과 경험을 공유해요</p>
+                  <p>매주 정기 스터디를 통해,<br>정해진 주제에 대한 발표하고,<br />지식과 경험을 공유해요</p>
                 </div>
               </v-card>
             </div>
@@ -124,11 +126,72 @@
       </div>
       <v-container></v-container>
       <div class="text-center">
-      <h1>저는 이런 기술 스택을 사용해요</h1>
-    </div>
+        <h1>저는 이런 기술 스택을 사용해요</h1>
+        <v-container></v-container>
+        <div style="width: 100vw; position: relative; left: 50%; transform: translateX(-50%);">
+
+          <Carousel :itemsToShow="4.5" :wrapAround="true" :transition="1000" :autoplay="1">
+            <Slide v-for="item in techStacks" :key="item">
+              <v-img :src="item.src" :alt="item.name" height="100px" />
+            </Slide>
+          </Carousel>
+        </div>
+
+      </div>
     </div>
   </div>
 </template>
 
 <script setup>
+import 'vue3-carousel/dist/carousel.css'
+import { Carousel, Slide } from 'vue3-carousel'
+let techStacks = [
+  {
+    name: "FastApi",
+    src: require("../assets/stacks/fastApi.png"),
+  },
+  {
+    name: "ec2.png",
+    src: require("../assets/stacks/ec2.png"),
+  },
+  {
+    name: "springBoot",
+    src: require("../assets/stacks/spring.png"),
+  },
+  {
+    name: "html5",
+    src: require("../assets/stacks/html5.png"),
+  },
+  {
+    name: "http",
+    src: require("../assets/stacks/http.png"),
+  },
+  {
+    name: "java",
+    src: require("../assets/stacks/java.png"),
+  },
+  {
+    name: "mysql",
+    src: require("../assets/stacks/mysql.jpeg"),
+  },
+  {
+    name: "redis",
+    src: require("../assets/stacks/redis.png"),
+  }
+]
+
+// let breakpoints = {
+//   320: {
+//     slidesPerView: 2,
+//     spaceBetween: 10
+//   },
+//   768: {
+//     slidesPerView: 2,
+//     spaceBetween: 20
+//   },
+//   1024: {
+//     slidesPerView: 3,
+//     spaceBetween: 30
+//   }
+// }
 </script>
