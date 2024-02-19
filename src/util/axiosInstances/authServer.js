836f74d159c5b8c528ff3b4e850950733655b529
iknowca <iknowca@gmail.com>
2024-02-19 17:40:39 +0900

    add: axios interceptor를 사용한 401 토큰 재발행 요청 기능 구현

--- src/util/axiosInstances/authServer.js
+++ src/util/axiosInstances/authServer.js
@@ -1,7 +1,27 @@
 import axios from 'axios';
+import router from '@/router';
+import { useAccountStore } from '@/stores/account';
 
 const authServerAxios = axios.create({
     baseURL: process.env.VUE_APP_AUTH_SERVER_BASE_URL,
 });
-authServerAxios.defaults.withCredentials = true;
+
+authServerAxios.interceptors.response.use((res) => {return res}, 
+    async (error) => {
+        if  (error.response.status === 401 && error.response?.data.message==="Token is not found" && error.config.url !== '/jwt/refresh') {
+            return await authServerAxios.get('/jwt/refresh', {withCredentials: true})
+            .then((res) => {
+                authServerAxios.defaults.headers.common['Authorization'] = `${res.data.accessToken}`;
+                return authServerAxios(error.config)
+            })
+            .catch(() => {
+                alert('로그인이 만료되었습니다.\n다시 로그인해주세요.');
+                useAccountStore().setLoginStatus(false);
+                router.push('/account/login')
+            });
+        } else {
+            return Promise.reject(error);
+        }
+    }
+)
 export default authServerAxios;
