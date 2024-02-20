fe626a94823b9da914b114537add155e0e0e2c97
iknowca <iknowca@gmail.com>
2024-02-20 14:21:45 +0900

    fix: interceptor를 사용한 자동 token refresh 기능 구현

--- src/util/axiosInstances/authServer.js
+++ src/util/axiosInstances/authServer.js
@@ -6,22 +6,42 @@ const authServerAxios = axios.create({
     baseURL: process.env.VUE_APP_AUTH_SERVER_BASE_URL,
 });
 
-authServerAxios.interceptors.response.use((res) => {return res}, 
+authServerAxios.interceptors.response.use((res) => { return res },
     async (error) => {
-        if  (error.response.status === 401 && error.response?.data.message==="Token is not found" && error.config.url !== '/jwt/refresh') {
-            return await authServerAxios.get('/jwt/refresh', {withCredentials: true})
-            .then((res) => {
-                authServerAxios.defaults.headers.common['Authorization'] = `${res.data.accessToken}`;
-                return authServerAxios(error.config)
-            })
-            .catch(() => {
-                alert('로그인이 만료되었습니다.\n다시 로그인해주세요.');
-                useAccountStore().setLoginStatus(false);
-                router.push('/account/login')
-            });
+        if (authServerAxios.refresh === true) {
+            return await handleRequest(error);
+        }
+        if (error.response.status === 401 && error.response?.data.message === "Token is not found" && error.config.url !== '/jwt/refresh') {
+            authServerAxios.refresh = true;
+            return await authServerAxios.get('/jwt/refresh', { withCredentials: true })
+                .then((res) => {
+                    authServerAxios.defaults.headers.common['Authorization'] = `${res.data.accessToken}`;
+                    authServerAxios.refresh = false;
+                    return authServerAxios(error.config)
+                })
+                .catch(() => {
+                    alert('로그인이 만료되었습니다.\n다시 로그인해주세요.');
+                    useAccountStore().setLoginStatus(false);
+                    router.push('/account/login')
+                });
         } else {
             return Promise.reject(error);
         }
     }
 )
+
+function waitForRefresh() {
+    return new Promise(resolve => {
+        const checkInterval = setInterval(() => {
+            if (authServerAxios.refresh === false) {
+                clearInterval(checkInterval);
+                resolve();
+            }
+        }, 100);
+    });
+}
+async function handleRequest(error) {
+    await waitForRefresh();
+    return authServerAxios(error.config);
+}
 export default authServerAxios;
